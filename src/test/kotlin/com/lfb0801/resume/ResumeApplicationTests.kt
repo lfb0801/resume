550bd060f918d03c3a8170055257789303dfc2a1
Lloyd van Zaalen <lfb0801@github.com>
2021-01-04 00:48:38 +0100

    initial commit

---
+++ src/test/kotlin/com/lfb0801/resume/ResumeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lfb0801.resume
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResumeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
